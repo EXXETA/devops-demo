eb3303101d4732c8ae031642ee081638c47a26c5
Georg Walther <georg.walther@exxeta.com>
2018-03-01 12:03:27 +0100

    Try more Chrome options to avoid exit

--- integration_tests/conftest.py
+++ integration_tests/conftest.py
@@ -11,6 +11,8 @@ def chrome():
     chrome_options = Options()
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--disable-gpu')
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--single-process')
 
     try:
         driver = webdriver.Chrome(
